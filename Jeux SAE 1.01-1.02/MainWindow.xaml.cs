aeea6064327eaa0d218201cc133a3ad5c6cc2804
Nathan Romeri <Nathan.Romeri@etu.univ-smb.fr>
2024-01-10 12:46:24 +0100

    salu

--- Jeux SAE 1.01-1.02/MainWindow.xaml.cs
+++ Jeux SAE 1.01-1.02/MainWindow.xaml.cs
@@ -42,7 +42,9 @@ namespace Jeux_SAE_1._01_1._02
 
 
 
-        private void MainWindow__KeyDown(object sender, KeyEventArgs e)
+     
+
+        private void Rectangle_KeyDown(object sender, KeyEventArgs e)
         {
             // Déplacez le personnage en fonction de la touche appuyée
             switch (e.Key)
@@ -60,11 +62,6 @@ namespace Jeux_SAE_1._01_1._02
                     Canvas.SetLeft(personnage, Canvas.GetLeft(personnage) + 10);
                     break;
             }
-        }
-
-        private void Rectangle_KeyDown(object sender, KeyEventArgs e)
-        {
-
         }
     }
 }
