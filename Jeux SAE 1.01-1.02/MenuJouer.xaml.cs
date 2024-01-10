b9aaf185416bcd0fc3759a09e97d426d06b4482f
Nathan Romeri <Nathan.Romeri@etu.univ-smb.fr>
2024-01-10 08:45:22 +0100

    ajout fenetre choix niveau et menu jouer

--- Jeux SAE 1.01-1.02/MenuJouer.xaml.cs
+++ Jeux SAE 1.01-1.02/MenuJouer.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,28 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Jeux_SAE_1._01_1._02
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour MenuJouer.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuJouer : Window
     {
-        public MainWindow()
+        public MenuJouer()
         {
             InitializeComponent();
-            MenuJouer fenetreMenu = new MenuJouer();
-            fenetreMenu.ShowDialog();
-
-            ChoixNiveau fenetreNiveau = new ChoixNiveau();
-            fenetreNiveau.ShowDialog();
-
-
+           
         }
-
-      
     }
 }
