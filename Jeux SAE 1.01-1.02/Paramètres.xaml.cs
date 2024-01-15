b9e98ffa6f8a9fe8199b4071ec38a80ffa284875
Noa Orand <Noa.Orand@etu.univ-smb.fr>
2024-01-15 15:47:06 +0100

    modifs parametres

---
+++ Jeux SAE 1.01-1.02/Paramètres.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Jeux_SAE_1._01_1._02
+{
+    /// <summary>
+    /// Logique d'interaction pour Paramètres.xaml
+    /// </summary>
+    public partial class Paramètres : Window
+    {
+        public Paramètres()
+        {
+            InitializeComponent();
+        }
+    }
+}
